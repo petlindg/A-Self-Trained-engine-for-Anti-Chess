3a626d28648860b982d66575585c4319f25e6d80
likr0012 <liam.kral0012@gmail.com>
2024-02-27 18:02:50 +0100

    Added a class that is able to run two models against each other (WIP)

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ checkpoint_path = "checkpoints/checkpoint.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
 class TrainingData:
-    def __init__(self, max_buffer_size, initial_state, tree_iterations, training_iterations, games):
+    def __init__(self, max_buffer_size, initial_state, tree_iterations, training_iterations, games, model_ref):
         # a buffer with a max size operating on a first in first out principle
         # during the training process, the oldest data will be replaced with the newest data
         # inside this buffer
@@ -25,6 +25,7 @@ class TrainingData:
         self.training_iterations = training_iterations
         self.games_per_iteration = games
         self.evaluation_result = []
+        self.model_ref = model_ref
     # method that will continually generate training data through self play while self.training is true
     def train(self):
         # main training loop, creates a game, runs the game and saves the game data - repeat
@@ -38,7 +39,7 @@ class TrainingData:
                 state_result = game.check_end_state()
                 tree = None
                 while type(state_result) == bool:
-                    tree = game.make_move(tree)
+                    tree = game.make_move(self.model_ref, tree)
                     state_result = game.check_end_state()
                     #print(game.current_state)
 
@@ -52,7 +53,7 @@ class TrainingData:
 
             # after a training iteration is complete, enough play data has been generated to fit the model once
             self.fit_data()
-            model.save_weights(checkpoint_path)
+            self.model_ref.save_weights(checkpoint_path)
 
     def test(self):
         initial_game = Game(self.initial_state, self.tree_iterations)
@@ -61,7 +62,7 @@ class TrainingData:
         state_result = game.check_end_state()
         tree = None
         while type(state_result) == bool:
-            tree = game.make_move(tree)
+            tree = game.make_move(self.model_ref, tree)
             state_result = game.check_end_state()
             print(game.current_state)
 
@@ -91,14 +92,14 @@ class TrainingData:
         # transforming the now shuffled list of tuples into two separate lists
         dists_train, vs_train = zip(*y_train)
         dists_test, vs_test = zip(*y_test)
-        model.fit(np.array(X_train),
+        self.model_ref.fit(np.array(X_train),
                   [np.array(dists_train), np.array(vs_train)],
                   epochs=50,
                   verbose=0,
                   batch_size=16
                   )
 
-        self.evaluation_result.append(model.evaluate(np.array(X_test),
+        self.evaluation_result.append(self.model_ref.evaluate(np.array(X_test),
                                         [np.array(dists_test), np.array(vs_test)]
                                         ))
 
@@ -165,13 +166,13 @@ class Game:
         else:
             return True
 
-    def make_move(self, old_tree=None):
+    def make_move(self, model_ref, old_tree=None):
         """
         Performs a move for the current game by creating an mcts tree
 
         :return: None
         """
-        tree = MCTS(self.current_state, self.tree_iterations, model, old_tree)
+        tree = MCTS(self.current_state, self.tree_iterations, model_ref, old_tree)
         tree.run()
         self.time_prediction += tree.time_prediction
         potential_nodes = tree.root_node.children
@@ -197,10 +198,159 @@ class Game:
         self.current_state.move(best_move)
         return tree.select_child(best_move)
 
+class EvaluateModel:
+    '''Class representing the evaluation of two models playing against one another'''
+    def __init__(self, initial_state, tree_iterations, model_1, model_2, games_to_play):
+        self.initial_state = initial_state
+        self.current_state = None
+        self.model_1 = model_1
+        self.model_2 = model_2
+        self.tree_iterations = tree_iterations
+        self.games_to_play = games_to_play
+        self.color_model_1 = chess.Color.WHITE
+        self.color_model_2 = chess.Color.BLACK
+        # trees representing each of the models
+        self.old_tree_1 = None
+        self.old_tree_2 = None
+        self.wins_model_1 = 0
+        self.wins_model_2 = 0
+        self.draws = 0
+
+    def swap_colors(self):
+        temp_color = self.color_model_1
+        self.color_model_1 = self.color_model_2
+        self.color_model_2 = temp_color
+
+    def run(self):
+        '''Method to run the evaluation
+
+        :return: None
+        '''
+        game_counter = 0
+        while game_counter < self.games_to_play:
+            self.play_game()
+            #print('game ended')
+            #print(self.current_state)
+            game_counter += 1
+            self.swap_colors()
+        print(f'winrate white: {self.wins_model_1 / (self.wins_model_2 + self.wins_model_1 + self.draws)} | '
+              f'winrate black: {self.wins_model_2 / (self.wins_model_2 + self.wins_model_1 + self.draws)} | '
+              f'drawrate: {self.draws/(self.wins_model_2 + self.wins_model_1 + self.draws)}')
+
+
+    def play_game(self):
+        '''Plays a single game between the two networks
+
+        :return: None
+        '''
+        # reset the state variables back to zero at the start
+        self.current_state = copy.deepcopy(self.initial_state)
+        self.old_tree_1 = None
+        self.old_tree_2 = None
+        print(self.old_tree_1)
+        print(self.old_tree_2)
+
+        while check_end_state(self.current_state):
+            #print(self.current_state)
+            # if the first model is the one to move
+            if self.color_model_1 == self.current_state.player_to_move:
+                # update the tree for model 1
+                self.old_tree_1, move = self.model_move(self.model_1, self.old_tree_1)
+                # if there is a tree for model 2 then update it as well with the move that model 1 made
+                if self.old_tree_2 is not None:
+                    self.old_tree_2 = self.old_tree_2.select_child(move)
+
+            # if the second model is the one to move
+            else:
+                # update the tree and get the move that model 2 made
+                self.old_tree_2, move = self.model_move(self.model_2, self.old_tree_2)
+                # if the first model has a previous tree then update that tree with the move that model 2 performed
+                if self.old_tree_1 is not None:
+                    self.old_tree_1 = self.old_tree_1.select_child(move)
+
+        # fetch the status when the game has ended
+        # and depending on what happened, increment the variables
+        status = self.current_state.get_game_status()
+        if status == 0:
+            if self.color_model_1 == chess.Color.WHITE:
+                self.wins_model_1 += 1
+            else:
+                self.wins_model_2 += 1
+        elif status == 1:
+            if self.color_model_1 == chess.Color.BLACK:
+                self.wins_model_1 += 1
+            else:
+                self.wins_model_2 += 1
+
+        elif status == 2:
+            self.draws += 1
+
+    def model_move(self, model_ref, old_tree=None):
+        '''Method for performing a single move for a given model
+
+        :param model_ref: Reference to the model object
+        :param old_tree: class Node, root node for the currently built up tree, None by default
+        :return: (Class Node, Class Move), returns the updated old_tree and the Move that the model decided on
+        '''
+        tree = MCTS(self.current_state, self.tree_iterations, model_ref, old_tree)
+        tree.run()
+        potential_nodes = tree.root_node.children
+        max_visit = 0
+        best_move = None
+        # iterate through all the possible children to the current state
+        # and select the one with the highest visit count, this is the move to perform
+        mcts_dist = []
+        visit_total = 0
+        for node in potential_nodes:
+            visit_total += node.visits
+            mcts_dist.append((node.visits, node.move))
+            if node.visits > max_visit:
+                max_visit = node.visits
+                best_move = node.move
+
+        # debug, best_move should never be None, if it is None then the game has ended without ruleset knowing it has ended
+        if best_move is None:
+            print(self.current_state)
+        print(best_move)
+        self.current_state.move(best_move)
+        return tree.select_child(best_move), best_move
+
+
+
+def check_end_state(current_state):
+    '''
+    Checks if the game has reached a final state
+
+    :return: Bool, if the game is over then it returns true
+    '''
+
+    status = current_state.get_game_status()
+
+    if status != 3:
+        game_over = False
+    else:
+        game_over = True
+
+    if status == 0:
+        winner = chess.Color.WHITE
+        print('===============================')
+        print('           white wins')
+        print('===============================')
+    elif status == 1:
+        winner = chess.Color.BLACK
+        print('===============================')
+        print('           black wins')
+        print('===============================')
+    elif status == 2:
+        winner = 'draw'
+        print('===============================')
+        print('             draw')
+        print('===============================')
+    return game_over
 
 class PlayGame:
     '''Class representing a single game against the AI via commandline'''
-    def __init__(self, initial_state, tree_iterations):
+    def __init__(self, initial_state, tree_iterations, model_ref):
         self.current_state = initial_state
         self.game_over = False
         self.game_history = []
@@ -208,36 +358,8 @@ class PlayGame:
         self.time_prediction = 0
         self.player_color = chess.Color.BLACK
         self.old_tree = None
-    def check_end_state(self):
-        '''
-        Checks if the game has reached a final state
+        self.model_ref = model_ref
 
-        :return: a list of tuples [(state, mcts_distribution, reward)]
-        '''
-
-        status = self.current_state.get_game_status()
-
-        if status != 3:
-            game_over = False
-        else:
-            game_over = True
-
-        if status == 0:
-            winner = chess.Color.WHITE
-            print('===============================')
-            print('           white wins')
-            print('===============================')
-        elif status == 1:
-            winner = chess.Color.BLACK
-            print('===============================')
-            print('           black wins')
-            print('===============================')
-        elif status == 2:
-            winner = 'draw'
-            print('===============================')
-            print('             draw')
-            print('===============================')
-        return game_over
     def player_move(self):
         """Takes player input and performs the players move when it is the players turn
 
@@ -272,7 +394,7 @@ class PlayGame:
 
                 :return: None
                 """
-        tree = MCTS(self.current_state, self.tree_iterations, model, old_tree)
+        tree = MCTS(self.current_state, self.tree_iterations, self.model_ref, old_tree)
         tree.run()
         self.time_prediction += tree.time_prediction
         potential_nodes = tree.root_node.children
@@ -306,7 +428,7 @@ class PlayGame:
         """
         time.sleep(10)
         print(self.current_state)
-        while self.check_end_state():
+        while check_end_state(self.current_state):
 
 
             if self.current_state.player_to_move == self.player_color:
@@ -405,7 +527,7 @@ def translate_moves_to_output(mcts_dist):
 
 def main():
     model_config = NeuralNetwork(input_shape=INPUT_SHAPE, output_shape=OUTPUT_SHAPE)
-    global model
+
     model = model_config.build_nn()
     try:
         model.load_weights(checkpoint_path)
@@ -413,12 +535,12 @@ def main():
         pass
     starting_board = chess.Chessboard()
     starting_board.init_board_test_2()
-    training_session = TrainingData(20, starting_board, 180, 40, 5)
+    training_session = TrainingData(20, starting_board, 180, 40, 5, model_ref=model)
     training_session.train()
 
 def main_play():
     model_config = NeuralNetwork(input_shape=INPUT_SHAPE, output_shape=OUTPUT_SHAPE)
-    global model
+
     model = model_config.build_nn()
     try:
         model.load_weights(checkpoint_path)
@@ -426,9 +548,27 @@ def main_play():
         pass
     starting_board = chess.Chessboard()
     starting_board.init_board_test_2()
-    game = PlayGame(starting_board, 160)
+    game = PlayGame(starting_board, 160, model_ref=model)
+    game.run()
+
+
+
+
+
+def main_evaluate():
+    model_config_1 = NeuralNetwork(input_shape=INPUT_SHAPE, output_shape=OUTPUT_SHAPE)
+    model_config_2 = NeuralNetwork(input_shape=INPUT_SHAPE, output_shape=OUTPUT_SHAPE)
+
+    model_1 = model_config_1.build_nn()
+
+    model_2 = model_config_2.build_nn()
+    model_2.load_weights(checkpoint_path)
+    starting_board = chess.Chessboard()
+    starting_board.init_board_test_2()
+    game = EvaluateModel(starting_board, 160, model_1, model_2, 20)
     game.run()
 
 if __name__ == "__main__":
     #main()
-    main_play()
+    #main_play()
+    main_evaluate()
