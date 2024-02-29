111ef302bb92bfa11fc33799113ab6b35042ccd0
likr0012 <liam.kral0012@gmail.com>
2024-02-29 22:20:06 +0100

    added a separate file containing code for specifically training the neural network

---
+++ Train.py
@@ -0,0 +1,70 @@
+import copy
+from collections import deque
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+from config import max_buffer_size, training_iterations, games_per_iteration, checkpoint_path
+from config import epochs, batch_size, verbosity
+from Game import TrainingGame
+
+
+class Training:
+    def __init__(self, initial_state, model):
+        self.buffer = deque(maxlen=max_buffer_size)
+        self.initial_state = initial_state
+        self.model = model
+        self.evaluation_result = []
+
+    def train(self):
+        t_counter = 0
+        initial_game = TrainingGame(self.initial_state)
+
+        while t_counter < training_iterations:
+            game_counter = 0
+            while game_counter < games_per_iteration:
+                game = copy.deepcopy(initial_game)
+                results = game.run(model=self.model)
+                self.buffer.append(results)
+                game_counter += 1
+                self.fit_data()
+            t_counter += 1
+
+            self.model.save_weights(checkpoint_path)
+
+    def fit_data(self):
+        """Method that uses  the data stored in the buffer to fit the model
+           and also evaluates the model, using a train test split.
+
+        :return:
+        """
+        list_states = []
+        list_outputs = []
+        # flattening out the buffer of games into the input and output data lists
+        for game in self.buffer:
+            for (state, dist, v) in game:
+                list_states.append(state[0])
+                list_outputs.append((np.array(dist).flatten(), v))
+
+        print(len(self.buffer))
+
+        X_train, X_test, y_train, y_test = train_test_split(list_states, list_outputs, shuffle=True)
+        # transforming the now shuffled list of tuples into two separate lists
+        dists_train, vs_train = zip(*y_train)
+        dists_test, vs_test = zip(*y_test)
+        self.model.fit(np.array(X_train),
+                       [np.array(dists_train), np.array(vs_train)],
+                       epochs=epochs,
+                       verbose=verbosity,
+                       batch_size=batch_size
+                       )
+
+        self.evaluation_result.append(self.model.evaluate(np.array(X_test),
+                                      [np.array(dists_test), np.array(vs_test)]
+                                        ))
+
+
+
+
+
+
