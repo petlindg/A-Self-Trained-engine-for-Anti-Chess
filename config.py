57e11814c6ee2b77cf9abc818e259fc17c6c50eb
likr0012 <liam.kral0012@gmail.com>
2024-02-29 23:20:52 +0100

    added comments to the config file

--- config.py
+++ config.py
@@ -9,18 +9,25 @@ exploration_constant: float = 4
 'constant defining how many iterations per tree will be performed'
 tree_iterations: int = 200
 
+'output dimensions for the neural network'
 output_representation = (1,1,8,8,76)
 
+'maximum number of games saved in the FIFO queue that the model uses as a dataset'
 max_buffer_size = 100
 
+'total number of iterations that are performed before the training stops'
 training_iterations = 5
 
+'total number of games that are performed per training iteration'
 games_per_iteration = 5
 
+'epochs during the training process'
 epochs = 30
 
+'batch size for the training process'
 batch_size = 16
 
+'verbosity of the training process (how much terminal output is displayed)'
 verbosity = 0
 
 checkpoint_path = "checkpoints/checkpoint.ckpt"
