57fecea4bd3a94fa4e7d7350e3f872f57edd43e8
likr0012 <liam.kral0012@gmail.com>
2024-03-09 00:58:50 +0100

    added functionality to generate a dataset from 'random' move mcts.

--- Game/dataset_generator.py
+++ Game/dataset_generator.py
@@ -1,26 +1,49 @@
+import time
+
+import config
 from TrainingGame import TrainingGame
-from chess import Chessboard
+from chess import Chessboard, Color
 import pickle
+import bz2
+from state_generator import generate_random_state
 
 
-def main():
+def run_once(original_fen, random_state=False):
+    data = []
+    start = time.time()
+    try:
+        with bz2.BZ2File('trainingdata.bz2', 'r') as f:
+            data = pickle.load(f)
+    except Exception as e:
+        print(e)
 
-    final_data = []
     for i in range(0, 100):
-        state = Chessboard("k7/8/8/8/8/8/8/7R w - 0 1")
+        if random_state:
+            fen_str = generate_random_state(config.piece_list)
+        else:
+            fen_str = original_fen
+        state = Chessboard(fen_str)
         game = TrainingGame(initial_state=state,
                             model=None)
         game.run()
-        final_data.append(game.get_history())
+        data.append(game.get_history())
 
-    with open('trainingdata.pkl', 'wb') as outp:
-        pickle.dump(final_data, outp, pickle.HIGHEST_PROTOCOL)
+    with bz2.BZ2File('trainingdata.bz2', 'w') as f:
+        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+    end = time.time()
+    print(len(data), ' | ', end-start, ' s')
+
+def main():
+
+    while True:
+        run_once("k7/8/8/8/8/8/8/7R w - 0 1")
+
 
 def read_data():
 
     with open('trainingdata.pkl', 'rb') as inp:
         data = pickle.load(inp)
         print(data)
+
 if __name__ == '__main__':
     main()
-    read_data()
