b3fea80d89c2be54577efda95aafeaf20874c29e
likr0012 <liam.kral0012@gmail.com>
2024-03-09 00:58:19 +0100

    changed some config values and added learning rate and piece list as arguments

--- config.py
+++ config.py
@@ -3,11 +3,12 @@
 Global configuration values for the program
 ===========================================
 '''
+from chess import Color, Piece
 
 'constant defining exploration vs exploitation of the tree'
 exploration_constant: float = 4
 'constant defining how many iterations per tree will be performed'
-tree_iterations: int = 200
+tree_iterations: int = 800
 
 'output dimensions for the neural network'
 output_representation = (1,1,8,8,76)
@@ -27,6 +28,8 @@ epochs = 30
 'batch size for the training process'
 batch_size = 16
 
+'Learning rate for the network'
+learning_rate = 0.2
 'verbosity of the training process (how much terminal output is displayed)'
 verbosity = 0
 
@@ -35,3 +38,5 @@ checkpoint_path = "checkpoints/checkpoint.ckpt"
 'evaluation method is the method by which the MCTS calculates P values for nodes during the expansion phase'
 'if the model is not designated as an argument. Can be one of: ones, dirichlet'
 evaluation_method = 'dirichlet'
+
+piece_list = [(Color.BLACK, Piece.KING), (Color.WHITE, Piece.ROOK)]
