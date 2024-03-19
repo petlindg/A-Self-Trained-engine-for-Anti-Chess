21e4ba06c037ce0db56f0521fa83353fc6d56ca2
likr0012 <liam.kral0012@gmail.com>
2024-03-19 11:53:19 +0100

    added comments

--- Game/dataset_generator.py
+++ Game/dataset_generator.py
@@ -9,18 +9,32 @@ import bz2
 from state_generator import generate_random_state
 
 
+def run_games(original_fen, random_state=False, load_data=False, games_played=100):
+    """A function that takes a fen notation string and runs games of that fen state
+    without the neural network, has an additional parameter for whether to randomize the states
+    where the randomized board is based on the config.piece_list
 
-def run_once(original_fen, random_state=False):
+    :param original_fen: String, Fen state that will be used when random_state is set to False
+    :param random_state: Boolean, Whether to use random states from the piece_list or use original_fen
+    :param load_data: Boolean, Whether to load data from the previous training data file,
+    if false it will overwrite the old file
+    :param games_played: Int, Number of games that will be performed, default value 100.
+    :return: None
+    """
     data = []
     start = time.time()
-    try:
-        with bz2.BZ2File('trainingdata.bz2', 'r') as f:
-            data = pickle.load(f)
-    except Exception as e:
-        print(e)
+    # if we want to load the old data
+    if load_data:
+        try:
+            with bz2.BZ2File('trainingdata.bz2', 'r') as f:
+                data = pickle.load(f)
+        except Exception as e:
+            print(e)
 
     counter = 0
-    while counter < 1:
+    # play games until enough games have been performed
+    while counter < games_played:
+        # generate a random fen notation if random_state is true
         if random_state:
             fen_str = generate_random_state(config.piece_list)
         else:
@@ -29,23 +43,29 @@ def run_once(original_fen, random_state=False):
         game = TrainingGame(initial_state=state,
                             model=None)
         result = game.run()
-        if result == Color.WHITE:
+        # if the result is either a win for white or a win for black, add it to the data
+        # this is done so that we avoid draws in our training data.
+        if result == Color.WHITE or result == Color.BLACK:
             data.append(game.get_history())
             counter += 1
             print(result)
 
+    # save the result
     with bz2.BZ2File('trainingdata.bz2', 'w') as f:
         pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
     end = time.time()
     print(len(data), ' | ', end-start, ' s')
 
+
 def main():
-
-
-    run_once("k7/8/8/8/8/8/8/7R w - 0 1")
+    run_games("k7/8/8/8/8/8/8/7R w - 0 1")
 
 
 def read_data():
+    """Function that reads the data from the training data file that has been saved. useful for debugging.
+
+    :return: None
+    """
     with bz2.BZ2File('trainingdata.bz2', 'r') as f:
         data = pickle.load(f)
         for game in data:
@@ -54,5 +74,5 @@ def read_data():
                 print(v)
 
 if __name__ == '__main__':
-    read_data()
-    #main()
+    #read_data()
+    main()
