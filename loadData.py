345eb39da6353dbe48bbbbcf46d1c225da0fbafc
Petter Lindgren <petlindg@net.chalmers.se>
2024-03-19 02:26:10 +0100

    added a way to print generated data

--- loadData.py
+++ loadData.py
@@ -1,13 +1,18 @@
+import bz2
 import pickle
 
 def main():
-    fp = "trainingData/training_data_2024-03-13T13"
-    file = open(fp, 'rb')
-    game_history = pickle.load(file)
-    for (state, p_list, winner) in game_history:
-        print(state)
-        print(p_list)
-        print(winner)
+    data = []
+    try:
+        with bz2.BZ2File('trainingdata.bz2', 'r') as f:
+            data = pickle.load(f)
+    except Exception as e:
+        print(e)
+    for game in data:
+        for (state, p_list, winner) in game:
+            print(state)
+            print(p_list)
+            print(winner)
 
 if __name__=='__main__':
     main()
