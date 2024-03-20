a5c45203a2815f72aa1d7b66eaa57219a8c5947c
Liam Kral <liamk@chalmers.se>
2024-03-20 19:57:14 +0100

    updated the neural network, it should be functional now.

--- nn_architecture.py
+++ nn_architecture.py
@@ -5,8 +5,9 @@ from keras.models import Model
 from keras.optimizers import Adam
 from keras.layers import Add
 from tensorflow.python.keras.engine.keras_tensor import KerasTensor
+import config
 
-# NEURAL NETWORK INPUTS 
+# NEURAL NETWORK INPUTS
 # 12 planes for the pieces (2 players of 6 pieces)
 # 2 planes for repetition
 # 1 plane for color
@@ -20,7 +21,7 @@ n = 8  # board size
 total_input_planes = (2 * 6 + 1) + (1 + 2 + 1)
 INPUT_SHAPE = (n, n, total_input_planes)
 
-# NEURAL NETWORK OUTOUTS 
+# NEURAL NETWORK OUTOUTS
 
 # the model has 2 outputs: policy and value
 # ouput_shape[0] should be the number of possible moves
@@ -39,11 +40,13 @@ OUTPUT_SHAPE = (8 * 8 * total_planes, 1)
 
 # NEURAL NETWORK PARAMETERS
 
-LEARNING_RATE = 0.2
+LEARNING_RATE = config.learning_rate
 # filters for the convolutional layers
 CONVOLUTION_FILTERS = 256
 # amount of hidden residual layers according to the alpha zero paper
-RESIDUAL_BLOCKS = 19
+RESIDUAL_BLOCKS = config.RESIDUAL_BLOCKS
+
+KERNEL_INITIALIZER = 'glorot_normal'
 
 
 class NeuralNetwork:
@@ -60,23 +63,29 @@ class NeuralNetwork:
         self.num_hidden_layers = RESIDUAL_BLOCKS
         self.convolution_filters = CONVOLUTION_FILTERS
 
-    def convolutional_layer(self, input_x) -> KerasTensor:
-        conv_layer = Conv2D(self.convolution_filters, kernel_size=(3, 3), strides=(1, 1), padding="same",
-                            data_format='channels_first', use_bias=False,
-                            kernel_initializer='zeros')(input_x)
-        conv_layer = BatchNormalization(axis=1)(conv_layer)
-        conv_layer = Activation('relu')(conv_layer)
+    def convolutional_layer(self, input_x, name) -> KerasTensor:
+        conv_layer = Conv2D(self.convolution_filters,
+                            kernel_size=(3, 3),
+                            name=f'{name}',
+                            padding="same",
+                            kernel_initializer=KERNEL_INITIALIZER
+                            )(input_x)
+
+        conv_layer = BatchNormalization(axis=1, name=f'{name}_BatchNorm')(conv_layer)
+        conv_layer = Activation('relu', name=f'{name}_relu')(conv_layer)
 
         return conv_layer
 
-    def residual_layer(self, input_x) -> KerasTensor:
-        conv1 = self.convolutional_layer(input_x)
-        conv2 = Conv2D(self.convolution_filters, kernel_size=(3, 3), strides=(1, 1), padding="same",
-                       data_format='channels_first', use_bias=False,
-                       kernel_initializer='zeros'
+    def residual_layer(self, input_x, name) -> KerasTensor:
+        conv1 = self.convolutional_layer(input_x, f'res_block_{name}_conv_1')
+        conv2 = Conv2D(self.convolution_filters,
+                       kernel_size=(3, 3),
+                       name=f'residual_block_{name}_conv_2',
+                       padding="same",
+                       kernel_initializer=KERNEL_INITIALIZER
                        )(conv1)
-        conv2 = BatchNormalization(axis=1)(conv2)
 
+        conv2 = BatchNormalization(axis=1, name=f'res_block_{name}_BatchNorm')(conv2)
         # skip connections
         """
         used to add the input of the block to its output.
@@ -84,36 +93,48 @@ class NeuralNetwork:
         bypasses the layers between the input and output of the residual block
 
         """
-        skip_connection = Add()([conv2, input_x])
-        res_layer = Activation('relu')(skip_connection)
+        skip_connection = Add(name=f'res_block_{name}_skip')([conv2, input_x])
+        res_layer = Activation('relu', name=f'res_block_{name}_relu')(skip_connection)
         return res_layer
 
-    def policy_network(self) -> Model:
-        policy_head = Sequential()
-        policy_head.add(Conv2D(2, (1, 1), strides=1, padding='same', data_format='channels_first',
-                               input_shape=(self.convolution_filters, *self.input_shape[1:]),
-                               kernel_initializer='zeros'))
-        policy_head.add(BatchNormalization(axis=1))
-        policy_head.add(Activation('relu'))
-        policy_head.add(Flatten())
-        policy_head.add(
-            Dense(self.output_shape[0], activation='softmax', name='policy_output', kernel_initializer='zeros'))
+    def policy_network(self, x) -> Model:
+        x = Conv2D(2, (1, 1),
+                   kernel_initializer=KERNEL_INITIALIZER,
+                   padding='same',
+                   name='policy_1_conv_input')(x)
 
-        return policy_head
+        x = BatchNormalization(axis=1, name='policy_2_BatchNorm')(x)
 
-    def value_network(self) -> Model:
-        value_head = Sequential()
-        value_head.add(Conv2D(1, (1, 1), strides=1, padding='same', data_format='channels_first',
-                              input_shape=(self.convolution_filters, *self.input_shape[1:]),
-                              kernel_initializer='zeros'))
-        value_head.add(BatchNormalization(axis=1))
-        value_head.add(Activation('relu'))
-        value_head.add(Flatten())
-        value_head.add(Dense(256, activation='relu',
-                             kernel_initializer='zeros'))  # Intermediate dense layer for deeper feature extraction
-        # Final dense layer outputs a single value with tanh activation for outcome prediction
-        value_head.add(Dense(1, activation='sigmoid', name='value_output', kernel_initializer='zeros'))
-        return value_head
+        x = Activation('relu', name='policy_3_relu')(x)
+
+        x = Flatten(name='policy_4_flatten')(x)
+
+        x = Dense(self.output_shape[0], activation='softmax', name='policy_output')(x)
+        return x
+
+    def value_network(self, x) -> Model:
+        x = Conv2D(1,
+                   (1, 1),
+                   padding='same',
+                   name='value_1_conv_input',
+                   input_shape=(self.convolution_filters, *self.input_shape[1:]),
+                   kernel_initializer=KERNEL_INITIALIZER)(x)
+
+        x = BatchNormalization(axis=1, name='value_2_BatchNorm')(x)
+
+        x = Activation('relu', name='value_3_relu')(x)
+
+        x = Flatten(name='value_4_flatten')(x)
+
+        x = Dense(self.convolution_filters,
+                  name='value_5_linear',
+                  activation='relu',
+                  kernel_initializer=KERNEL_INITIALIZER)(x)
+
+        x = Dense(1, activation='sigmoid',
+                  name='value_output',
+                  kernel_initializer=KERNEL_INITIALIZER)(x)
+        return x
 
     def build_nn(self) -> Model:
         """
@@ -124,20 +145,14 @@ class NeuralNetwork:
         input_feature = Input(shape=self.input_shape, name='input_feature')
 
         # Initial convolutional layer
-        x = self.convolutional_layer(input_feature)
+        x = self.convolutional_layer(input_feature, 'input')
 
         # Add residual layers
-        for _ in range(self.num_hidden_layers):
-            x = self.residual_layer(x)
-
-        model = Model(inputs=input_feature, outputs=x)
-
-        # Construct policy and value heads
-        policy_head = self.policy_network()
-        value_head = self.value_network()
+        for i in range(self.num_hidden_layers):
+            x = self.residual_layer(x, str(i + 1))
 
         # Instantiate the model with inputs and both outputs
-        model = Model(inputs=input_feature, outputs=[policy_head(x), value_head(x)])
+        model = Model(inputs=input_feature, outputs=[self.policy_network(x), self.value_network(x)])
 
         # Compile the model with specified losses, optimizer, and loss weights
         model.compile(
@@ -161,12 +176,7 @@ if __name__ == "__main__":
     # create the model
     model_config = NeuralNetwork(input_shape=INPUT_SHAPE, output_shape=OUTPUT_SHAPE)
     model = model_config.build_nn()
-
-    # print(model.summary())
-    nn_instance = NeuralNetwork(INPUT_SHAPE, OUTPUT_SHAPE)
-    policy_net = nn_instance.policy_network()
-    print(policy_net.summary())
-
+    print(model.summary())
 
 
 
