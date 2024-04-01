d2dd6b72d20b0b1306335650a54a61514737ca74
likr0012 <liam.kral0012@gmail.com>
2024-04-02 01:57:52 +0200

    cleaned up code

--- training_mp.py
+++ training_mp.py
@@ -24,9 +24,13 @@ class NeuralNetworkProcess(multiprocessing.Process):
         self.buffer = deque(maxlen=max_buffer_size)
         self.games_counter = 0
         self.model = None
+        self.evaluations = {'hits': 0, 'misses': 0}
 
     def run(self):
-        print('running nn process')
+        """Start the neural network process allowing it to process data
+
+        :return: None
+        """
         start_time = time.time()
         model_config = NeuralNetwork(input_shape=INPUT_SHAPE, output_shape=OUTPUT_SHAPE)
         self.model = model_config.build_nn()
@@ -35,34 +39,44 @@ class NeuralNetworkProcess(multiprocessing.Process):
             self.model.load_weights(checkpoint_path)
         except Exception as e:
             print('EXCEPTION, couldnt load weights ', e)
-        # accept new requests and store them
-        # once the stored values exceed a certain size
-        # process the requests
-        # once finished, return all the processes correctly
+
         while True:
-            # request_type is whether it is an evaluation or a finished game
+            # get the latest request from the queue
+            # based on the request type, perform some action
             request_type, uid, data = self.input_queue.get()
-            #print(f'request recieved: {request_type} | {uid} | {np.shape(data)}')
-            # if the request is an evaluation then data will be the state to be evaluated.
+
+            # if the request is an evaluation then store the data for later evaluation
+            # also check if the data already exists in the dictionary, if it does,
+            # return the value to the process which asked for it
             if request_type == 'eval':
-                self.list_uid.append(uid)
-                self.list_states.append(data)
+                dict_vals = self.evaluations.get(data.data.tobytes())
+                if dict_vals is not None:
+                    p, v = dict_vals
+                    output_queue = self.output_queues[uid]
+                    output_queue.put((p, v))
+                # if the data doesn't exist in the dictionary, add it to the internal storage for evaluation
+                # the internal data is split into two parts so that it can later be sent back to the correct worker
+                else:
+                    self.list_uid.append(uid)
+                    self.list_states.append(data)
 
             # if the request is a finished game, data will be the final result from the game
+            # therefore append it to our data buffer and increment our counter
             elif request_type == 'finished':
                 self.buffer.append(data)
                 self.games_counter += 1
                 print(f'game finished uid:{uid} | c: {self.games_counter}')
 
-            # if the list states is large enough
-            # perform the calculation on the set
+            # if the list of pending evaluation states is large enough
+            # perform the model evaluation on the list
             if len(self.list_states) >= self.max_size:
                 self._process_requests()
 
-            # if the accumulated game results reaches appropriate size, train the network
+            # if the accumulated game results reaches appropriate size, train the model
             if self.games_counter >= games_per_iteration:
                 self._train_network()
-                print(f'30 games done, {time.time() - start_time} s')
+                print(f'{time.time() - start_time} s')
+                time.sleep(30)
                 # 30 games in 83 seconds, 2.7s per game
 
     def _process_requests(self):
@@ -70,21 +84,20 @@ class NeuralNetworkProcess(multiprocessing.Process):
 
         :return: None
         """
+        # reshape the array to fit the model
         model_input = np.array(self.list_states).reshape((-1, 8, 8, 17))
-
         result = self.model.predict(model_input, verbose=0)
         list_ps = result[0]
         list_vs = result[1]
         result_zip = zip(list_ps, list_vs)
 
-
-        # go through the result and send it back to the waiting threads
-        for key, (p, v) in zip(self.list_uid, result_zip):
+        # go through the result list and send each individual result back to the corresponding process
+        for input_repr, key, (p, v) in zip(model_input, self.list_uid, result_zip):
+            self.evaluations[input_repr.data.tobytes()] = (p, v)
             out_queue = self.output_queues[key]
-            #print(np.shape(result_eval[0]), np.shape(result_eval[1]))
             out_queue.put((p, v))
 
-        # empty the now processed input list
+        # empty the now processed input lists
         self.list_uid = []
         self.list_states = []
 
@@ -106,7 +119,6 @@ class GameProcess(multiprocessing.Process):
 
     def run(self):
         # while the process is running, keep running training games
-        print(f'running worker process {self.uid}')
         while True:
             game = TrainingGame(initial_state=self.initial_state, outgoing_queue=self.outgoing_queue,
                                 incoming_queue=self.incoming_queue, uid=self.uid)
