6305d99fbd820a496847904dbf7f189ec6b143dc
likr0012 <liam.kral0012@gmail.com>
2024-04-04 00:56:46 +0200

    ???????

--- training_mp.py
+++ training_mp.py
@@ -247,10 +247,9 @@ class GameProcess(multiprocessing.Process):
         # while the process is running, keep running training games
         while True:
             chessboard = Chessboard(self.initial_state)
-            val = 5
             print('=========================================')
             print('=========================================')
-            print('ID: ', id(val), ' ', self.uid)
+            print('ID: ', id(self), ' ', self.uid)
             print('=========================================')
             print('=========================================')
             random_state = generate_random_state(config.piece_list)
