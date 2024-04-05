a0214cc16e6034408d0fbd1b89bfec72926e196d
josefjakobson <j.jakobson28@gmail.com>
2024-04-05 16:40:31 +0200

    changed stats collection to use json instead of pickle

--- Stats/training_stats_plotter.py
+++ Stats/training_stats_plotter.py
@@ -1,51 +1,42 @@
 import pickle
 from keras.callbacks import History
 import matplotlib.pyplot as plt
-
+import bz2
+import json
 
 class TrainingPlot:
     '''
     Class containing functionality to collect and plot training metrics
     '''
-    data = [[],[]]
+    data = {"loss": [], "accuracy": []}
 
     def load_data(self):
         '''
-        Takes a pickle file from specific directory and unpickles/loads the data into the class.
+        Takes a json file from specific directory and loads the data into the class.
 
         :return: None
         '''
-        try:
-            with open("Stats/training_metrics.p", "rb") as f:
-                while True:
-                    try:
-                        model: History = pickle.load(f)
-                        self.process_loaded_data(model)
-                    except EOFError:
-                        break
-        except FileNotFoundError:
-            print("Training metrics data has not been saved yet")
+        with open("./training_metrics.json", "r") as f:
+            self.data = json.load(f)
     
+    def save_history_to_instance(self, history: History):
+        metrics = history.history
+        self.data["loss"].append(metrics["loss"][-1])
+        self.data["accuracy"].append(metrics["accuracy"][-1])
 
-    def store_pickled_data(model: History):
-        with open("Stats/training_metrics.p", "ab") as f:
-            pickle.dumps(model, f)
+    def save_history_to_json(self):
+        with open("./training_metrics.json", "w") as f:
+            json.dump(self.data, f)
     
-    def process_loaded_data(self, model_history: History):
-        final_loss = model_history.history["loss"][-1]
-        final_acc = model_history.history["accuracy"][-1]
-        self.data[0].append(final_loss)
-        self.data[1].append(final_acc)
-
     def plot_loss(self):
-        plt.plot(self.data[0])
+        plt.plot(self.data["loss"])
         plt.title("Change in Loss over Training Iterations")
         plt.xlabel("Training Iteration")
         plt.ylabel("Final Loss Value")
         plt.show()
     
     def plot_accuracy(self):
-        plt.plot(self.data[1])
+        plt.plot(self.data["accuracy"])
         plt.title("Change in Accuracy over Training Iterations")
         plt.xlabel("Training Iteration")
         plt.ylabel("Final Accuracy Value")
