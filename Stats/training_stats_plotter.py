5d1aa20e749be48ba32b0da41a7013c886dbcd82
josefjakobson <j.jakobson28@gmail.com>
2024-04-05 14:29:01 +0200

    changed save_history to save as json instead of pickle

--- Stats/training_stats_plotter.py
+++ Stats/training_stats_plotter.py
@@ -2,56 +2,41 @@ import pickle
 from keras.callbacks import History
 import matplotlib.pyplot as plt
 import bz2
+import json
 
 class TrainingPlot:
     '''
     Class containing functionality to collect and plot training metrics
     '''
-    data = [[],[]]
+    data = {"loss": [], "accuracy": []}
 
     def load_data(self):
         '''
-        Takes a pickle file from specific directory and unpickles/loads the data into the class.
+        Takes a json file from specific directory and loads the data into the class.
 
         :return: None
         '''
-        try:
-            with bz2.BZ2File("./training_metrics.bz2", "rb") as f:
-                while True:
-                    try:
-                        model: dict = pickle.load(f)
-                        self.process_loaded_data(model)
-                    except EOFError:
-                        break
-        except FileNotFoundError:
-            print("Training metrics data has not been saved yet")
+        with open("./training_metrics.json", "r") as f:
+            self.data = json.load(f)
     
-    # def compress(self):
-    #     with open("./training_metrics.p", "rb") as f:
-    #         pickled = pickle.load(f)
-    #         compressed = bz2.compress(pickled)
-    #         with bz2.open("./training_metrics.bz2", "wb") as f:
-    #             f.write(compressed)
+    def save_history_to_instance(self, history: History):
+        metrics = history.history
+        self.data["loss"].append(metrics["loss"][-1])
+        self.data["accuracy"].append(metrics["accuracy"][-1])
 
-    def pickle_history(self, history: dict):
-        with open("./training_metrics.p", "ab") as f:
-            pickle.dump(history, f, pickle.HIGHEST_PROTOCOL)
+    def save_history_to_json(self):
+        with open("./training_metrics.json", "w") as f:
+            json.dump(self.data, f)
     
-    def process_loaded_data(self, model_history: dict):
-        final_loss = model_history["loss"][-1]
-        final_acc = model_history["accuracy"][-1]
-        self.data[0].append(final_loss)
-        self.data[1].append(final_acc)
-
     def plot_loss(self):
-        plt.plot(self.data[0])
+        plt.plot(self.data["loss"])
         plt.title("Change in Loss over Training Iterations")
         plt.xlabel("Training Iteration")
         plt.ylabel("Final Loss Value")
         plt.show()
     
     def plot_accuracy(self):
-        plt.plot(self.data[1])
+        plt.plot(self.data["accuracy"])
         plt.title("Change in Accuracy over Training Iterations")
         plt.xlabel("Training Iteration")
         plt.ylabel("Final Accuracy Value")
