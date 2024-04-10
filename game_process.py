9df5430aaeefe028196ef674ebdd166f73276b66
Liam Kral <liamk@chalmers.se>
2024-04-10 15:47:04 +0200

    added process files for training

---
+++ game_process.py
@@ -0,0 +1,47 @@
+import multiprocessing
+import random
+
+import config
+
+class GameProcess(multiprocessing.Process):
+    """
+    Class representing a single process that plays games using the neural network. 
+    This class communicates with and sends requests to the neural network process and recieves evaluations from the 
+    neural network.
+    """
+    def __init__(self, input_queue, output_queue, initial_state, uid):
+        """
+        The game process class has two queues, the incoming and the outcoming queue
+        the incoming queue is the data that the game process recieves from the neural network
+        and the outgoing queue is the queue that the game process puts data on, for the network process to handle
+        the uid is the unique identifier for the process, it is used for identification purposes. 
+        """
+        super(GameProcess, self).__init__()
+        self.outgoing_queue = input_queue
+        self.incoming_queue = output_queue
+        self.initial_state = initial_state
+        self.uid = uid
+
+    def run(self):
+        """
+        Function that continually plays games by sending requests to the neural network
+        """
+        from chess import Chessboard
+        from Game.TrainingGame import TrainingGame
+        from Game.state_generator import generate_random_state
+
+        random.seed()
+        # while the process is running, keep running training games
+        while True:
+            self.chessboard = Chessboard(self.initial_state)
+
+            random_state = generate_random_state(config.piece_list)
+            if config.random_state_generation:
+                game = TrainingGame(initial_state=Chessboard(random_state), outgoing_queue=self.outgoing_queue,
+                                    incoming_queue=self.incoming_queue, uid=self.uid)
+            else:
+                game = TrainingGame(initial_state=self.chessboard, outgoing_queue=self.outgoing_queue,
+                                    incoming_queue=self.incoming_queue, uid=self.uid)
+            result = game.run()
+
+            self.outgoing_queue.put(('finished', self.uid, (game.get_history(), result)))
---
+++ nn_process.py
@@ -0,0 +1,244 @@
+import bz2
+import multiprocessing
+import pickle
+import random
+import time
+from collections import deque
+
+import numpy as np
+import tensorflow
+from sklearn.model_selection import train_test_split
+
+from config import max_buffer_size, games_per_iteration, checkpoint_path
+from config import epochs, batch_size, train_split
+
+
+from nn_architecture import NeuralNetwork, INPUT_SHAPE, OUTPUT_SHAPE
+from Stats.training_stats_plotter import TrainingPlot
+
+class TrainingData:
+    """
+    Class representing the training data, it contains all of the necessary data to train and test the neural network 
+    """
+    def __init__(self):
+        train_size = int(train_split*max_buffer_size)
+        test_size = int((1-train_split)*max_buffer_size)
+        self.X_train = deque(maxlen= train_size)
+        self.X_test = deque(maxlen= test_size)
+        self.y_train = deque(maxlen= train_size)
+        self.y_test = deque(maxlen= test_size)
+
+    def add(self, X_train, X_test, y_train, y_test):
+        self.X_train += X_train
+        self.X_test += X_test
+        self.y_train += y_train
+        self.y_test += y_test
+
+class NeuralNetworkProcess(multiprocessing.Process):
+    """
+    Class that represents a single neural network process, it handles incoming requests from
+    the game processes and manages the training and execution of the neural network.
+    """
+    def __init__(self, input_queue, output_queues, model=None):
+        super(NeuralNetworkProcess, self).__init__()
+        self.input_queue = input_queue # shared queue for the input
+        self.output_queues = output_queues # dict of queues to send back, has UID as key and queue as value
+        self.list_uid = []
+        self.list_states = []
+        self.batch_size = len(output_queues) # number of states to process in a single batch
+
+        self.buffer = []
+        self.training_data = TrainingData()
+        self.games_counter = 0
+        self.model = model
+        self.evaluations = {'hits': 0, 'misses': 0}
+        self.eval_result = []
+        self.statistics = TrainingPlot()
+
+    def run(self):
+        """Start the neural network process allowing it to process data
+
+        :return: None
+        """
+        load_model = True
+        # if the past game data should be loaded or not
+        random.seed()
+        if load_model:
+            pass
+        else:
+            self._load_past_data()
+            start_time = time.time()
+            model_config = NeuralNetwork(input_shape=INPUT_SHAPE, output_shape=OUTPUT_SHAPE)
+            self.model = model_config.build_nn()
+            try:
+                #pass
+                self.model.load_weights(checkpoint_path)
+            except Exception as e:
+                print('EXCEPTION, couldnt load weights ', e)
+
+
+
+        while True:
+            # get the latest request from the queue
+            # based on the request type, perform some action
+            request_type, uid, data = self.input_queue.get()
+
+            # if the request is an evaluation then store the data for later evaluation
+            # also check if the data already exists in the dictionary, if it does,
+            # return the value to the process which asked for it
+            if request_type == 'eval':
+                dict_vals = self.evaluations.get(data.data.tobytes())
+                if dict_vals is not None:
+                    self.evaluations['hits'] += 1
+                    p, v = dict_vals
+                    output_queue = self.output_queues[uid]
+                    output_queue.put((p, v))
+                # if the data doesn't exist in the dictionary, add it to the internal storage for evaluation
+                # the internal data is split into two parts so that it can later be sent back to the correct worker
+                else:
+                    self.evaluations['misses'] += 1
+                    self.list_uid.append(uid)
+                    self.list_states.append(data)
+
+            # if the request is a finished game, data will be the final result from the game
+            # therefore append it to our data buffer and increment our counter
+            elif request_type == 'finished':
+                game_data, result = data
+                self.buffer.append(game_data)
+                self.games_counter += 1
+                print(f'{result}| game finished uid:{uid} | c: {self.games_counter}')
+
+            # if the list of pending evaluation states is large enough
+            # perform the model evaluation on the list
+            if len(self.list_states) >= self.batch_size:
+                self._process_requests()
+
+            # if the accumulated game results reaches appropriate size, train the model
+            if self.games_counter >= games_per_iteration:
+                self.games_counter = 0
+                self._split_data()
+                self._train_network()
+                print(f'{time.time() - start_time} s')
+                hits = self.evaluations['hits']
+                misses = self.evaluations['misses']
+                sum = hits+misses
+                print(f'hits: {hits} | {misses} | hitrate: {hits/sum} | total: {sum}')
+                self.evaluations.clear()
+                self.evaluations['hits'] = hits
+                self.evaluations['misses'] = misses
+
+            self.statistics.save_history_to_json()
+
+    def _load_past_data(self):
+        """
+        Internal function that loads the past training data from the previous training run, if it exists
+        This ensures that the model always has a large set of training data to avoid overfitting when 
+        stopping and restarting the training process.
+        """
+        data = TrainingData()
+        try:
+            with bz2.BZ2File('Game/training_data_class.bz2', 'r') as f:
+                data = pickle.load(f)
+        except:
+            print('couldnt load past data')
+        self.training_data = data
+
+    def _process_requests(self):
+        """Method to process the requested evaluations and return the results to all the linked processes.
+
+        :return: None
+        """
+        # reshape the array to fit the model
+        model_input = np.array(self.list_states).reshape((-1, 8, 8, 17))
+        result = self.model.predict(model_input, verbose=0)
+        list_ps = result[0]
+        list_vs = result[1]
+        result_zip = zip(list_ps, list_vs)
+
+        # go through the result list and send each individual result back to the corresponding process
+        for input_repr, key, (p, v) in zip(model_input, self.list_uid, result_zip):
+            self.evaluations[input_repr.data.tobytes()] = (p, v)
+            out_queue = self.output_queues[key]
+            out_queue.put((p, v))
+
+        # empty the now processed input lists
+        self.list_uid = []
+        self.list_states = []
+
+    def _split_data(self):
+        """
+        Internal function that takes the current list of games in the buffer and distributes the game data
+        to the training and the test sets that make up the training data class. This ensures that the test set data
+        is never used as training data.
+        """
+        list_states = []
+        list_outputs = []
+        for game in self.buffer:
+            for (state, dist, v) in game:
+                list_states.append(state[0])
+                list_outputs.append((np.array(dist).flatten(), v))
+        X_train, X_test, y_train, y_test = train_test_split(list_states, list_outputs,
+                                                            shuffle=True,
+                                                            train_size=train_split)
+        self.training_data.add(X_train, X_test, y_train, y_test)
+        self.buffer.clear()
+
+    def _train_network(self):
+        """Method to train the network from the accumulated games
+
+        :return: None
+        """
+
+        dists_train, vs_train = zip(*self.training_data.y_train)
+        dists_test, vs_test = zip(*self.training_data.y_test)
+
+        history = self.model.fit(np.array(self.training_data.X_train),
+                       [np.array(dists_train), np.array(vs_train)],
+                       epochs=epochs,
+                       verbose=1,
+                       batch_size=batch_size,
+                       validation_data=(np.array(self.training_data.X_test),[np.array(dists_test), np.array(vs_test)]),
+                       )
+
+        self.statistics.save_history_to_instance(history)
+
+
+        #eval = self.model.evaluate(np.array(self.training_data.X_test),
+        #                    [np.array(dists_test), np.array(vs_test)]
+        #                    )
+        #print(eval)
+        #self.eval_result.append(eval)
+
+        self.model.save_weights(checkpoint_path)
+        save_to_file('Game/training_data_class.bz2', self.training_data)
+
+
+def save_to_file(filename, data):
+    with bz2.BZ2File(filename, 'w') as f:
+        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+
+def h5_to_weights():
+    """
+    Function that creates a model, loads the .h5 file and saves the weights in the checkpoints path
+    """
+    model_config = NeuralNetwork(input_shape=INPUT_SHAPE, output_shape=OUTPUT_SHAPE)
+    model = model_config.build_nn()
+    try:
+        tensorflow.keras.models.load_model('saved_model/model.h5')
+    except Exception as e:
+        print('EXCEPTION, couldnt load model ', e)
+    
+    model.save_weights(checkpoint_path)
+
+def weights_to_h5():
+    """
+    Function that creates a model, loads the weights and saves the model as an .h5 file
+    """
+    model_config = NeuralNetwork(input_shape=INPUT_SHAPE, output_shape=OUTPUT_SHAPE)
+    model = model_config.build_nn()
+    try:
+        model.load_weights(checkpoint_path)
+    except Exception as e:
+        print('EXCEPTION, couldnt load model ', e)
+
+    model.save('saved_model/model.h5')
