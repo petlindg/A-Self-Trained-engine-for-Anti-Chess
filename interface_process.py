5a0f2171c23b51359fc0772d0059a0a78bd5ea2e
Petter Lindgren <petlindg@student.chalmers.se>
2024-05-08 11:11:05 +0200

    .

---
+++ interface_process.py
@@ -0,0 +1,23 @@
+from multiprocessing import Process, Queue
+from interface import ChessboardGUI
+from chess.chessboard import Chessboard
+from chess.move import Move
+
+class InterfaceProcess(Process):
+    chessboard:Chessboard
+    move_queue:Queue
+
+    def move(self, move:Move):
+        self.move_queue.put(move)
+        self.chessboard.move(move)
+        
+    def get_move(self):
+        move = self.move_queue.get()
+        self.chessboard.move(move)
+        return self.chessboard.get()
+
+    def __init__(self, move_queue:Queue, chessboard:Chessboard):
+        self.move_queue = move_queue
+        self.chessboard = chessboard
+
+        self.interface = ChessboardGUI(send_move=self.move, get_bitboards=self.get_move)
