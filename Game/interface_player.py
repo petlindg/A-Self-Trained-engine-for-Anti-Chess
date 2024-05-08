c9c2fdab78ada62c4e7f86b0d89078354e692e2c
Petter Lindgren <petlindg@student.chalmers.se>
2024-05-08 16:55:20 +0200

    Added InterfacePlayer class as a wrapper for the interface_process class that implements the methods required for it to work in the Game class

--- Game/interface_player.py
+++ Game/interface_player.py
@@ -1,26 +1,32 @@
 from multiprocessing import Queue, set_start_method
 import sys
-
-from interface_process import InterfaceProcess
 sys.path.append('..')
+
+from chess import Chessboard
+from interface_process import InterfaceProcess
 
 class InterfacePlayer:
     """
     A class representing an interface player
     """
-    def __init__(self, chessboard):
-        self.chessboard = chessboard
-        self.move_queue = Queue()
+    def __init__(self, chessboard:Chessboard):
         set_start_method('spawn')
-        
-        interface_process = InterfaceProcess(self.move_queue, self.chessboard)
+        self.chessboard = chessboard
+        self.outgoing_queue = Queue()
+        self.incoming_queue = Queue()
+        interface_process = InterfaceProcess(incoming_queue=self.incoming_queue,
+                                             outgoing_queue=self.outgoing_queue,
+                                             chessboard=self.chessboard)
         interface_process.daemon = True
         interface_process.start()
         
         
 
     def get_move(self):
-        return self.move_queue.get()
+        move = self.outgoing_queue.get()
+        self.outgoing_queue.empty()
+        return move
 
     def update(self, move):
-        self.move_queue.put(move)
+        self.chessboard.move(move)
+        self.incoming_queue.put(move)
