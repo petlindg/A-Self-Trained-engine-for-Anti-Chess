41599437531f295895ff4b74490e2fe8f665d00e
Petter Lindgren <petlindg@net.chalmers.se>
2024-05-12 22:37:29 +0200

    fixed stuff

--- model_eval/eval_models.py
+++ model_eval/eval_models.py
@@ -138,7 +138,7 @@ def main():
     print("0vs rvK")
     model_1 = tensorflow.keras.models.load_model('../saved_model/model_0_it.h5', compile=False)
     model_1.compile()
-    model_2 = tensorflow.keras.models.load_model('../saved_model/model_3k1r_playable.h5', compile=False)
+    model_2 = tensorflow.keras.models.load_model('../saved_model/model_rK.h5', compile=False)
     model_2.compile()
     win_count_model_1, win_count_model_2, draw_count, avg_p_legal_model_1, avg_p_legal_model_2, move_avg = eval_models(model_1, model_2, EVAL_GAMES, initial_state=Chessboard("r7/8/8/8/4K3/8/8/8 w - 0 1"))
     print(f"Result distribution [model 1|model 2|draw]: [{win_count_model_1}|{win_count_model_2}|{draw_count}]")
